28bf105b74b111f2bf7413f627bfe66c04e8e916
Ratchanon <ratchanon.p@cjworx.com>
2021-12-23 16:44:09 +0700

    Add comment

--- controllers/authController.go
+++ controllers/authController.go
@@ -2,10 +2,12 @@ package controllers
 
 import "github.com/gofiber/fiber"
 
+// Hello via '/'
 func Hello(c *fiber.Ctx) {
 	c.SendString("Hello world !")
 }
 
+// Post via '/post'
 func Post(c *fiber.Ctx) {
 	c.Status(200).JSON(&fiber.Map{
 		"message": "Hello!",
